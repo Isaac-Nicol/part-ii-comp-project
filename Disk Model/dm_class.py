59bf417af0fe3cf6047358ed1b5855264b99c716
Isaac Nicol <in279@cam.ac.uk>
2024-04-18 22:32:26 +0100

    actually added the cluster algorithm to the disk model as well, and made the mh work through the same methods

--- Disk Model/dm_class.py
+++ Disk Model/dm_class.py
@@ -1,3 +1,4 @@
+from collections import deque
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -25,8 +26,12 @@ class DiskModel:
         self.N = N
         self.L = L
         self.configuration = self.generate_configuration()
+        self.occupation = self.initialize_occupation_array()
         
     def generate_configuration(self):
+        '''
+        This generates an evenly spaced grid pattern and then fills it with disks up to self.N
+        '''
         # The configuration is only possible if the number of disks is less than a certain threshold
         if self.N > np.ceil(self.L/2 - 1)**2:
             self.N = np.ceil(self.L/2 - 1)**2  
@@ -61,15 +66,45 @@ class DiskModel:
         dy = min(dy, self.L - dy)
         return np.sqrt(dx**2 + dy**2) < 2
     
-    def valid_move(self, disk, new_disk):
+    def get_overlap(self, point):
+        '''Return a list of indices of disks that have overlap with point.'''
+        overlaps = []
+        for dx in range(-2,3):
+            x = (int(point[0]) + dx) % self.L
+            for dy in range(-2,3):
+                y = (int(point[1]) + dy) % self.L
+                index = self.occupation[x,y]
+                if index >= 0 and (((point[0]%1)-(self.configuration[index,0]%1)-dx)**2 +
+                                ((point[1]%1)-(self.configuration[index,1]%1)-dy)**2 < 4):
+                    overlaps.append(index)
+        return overlaps
+    
+    def valid_move(self, new_disk):
         '''
         Check if a move is valid
         '''
-        for i in range(self.N):
-            if i != disk and self.check_overlap(new_disk, self.configuration[i]):
-                return False
+        overlaps = self.get_overlap(new_disk)
+        if overlaps:
+            return False
         return True
     
+    def initialize_occupation_array(self):
+        '''
+        Create an LxL array with indices containing the disk number at that position, -1 if empty
+        '''
+        occupation = -np.ones((self.L, self.L), dtype='int')
+        for i, (x, y) in enumerate(self.configuration):
+            occupation[int(x),int(y)] = i
+        return occupation
+    
+    def clear_disk(self, index):
+        '''Remove disk from occupation array.'''
+        self.occupation[int(self.configuration[index, 0]), int(self.configuration[index, 1])] = -1
+
+    def add_disk(self, index):
+        '''Add disk to occupation array.'''
+        self.occupation[int(self.configuration[index, 0]),int(self.configuration[index, 1])] = index
+
     def metropolis_hastings(self, delta=1):
         '''
         Perform the Metropolis-Hastings algorithm to move a disk
@@ -83,11 +118,32 @@ class DiskModel:
         y_new = delta * (y + np.random.normal()) % self.L
         new_disk = np.array([x_new, y_new])
         
-        if self.valid_move(disk, new_disk):
+        if self.valid_move(new_disk):
+            self.clear_disk(disk)
             self.configuration[disk] = new_disk
+            self.add_disk(disk)
             return
         return
     
+    def disk_cluster_move(self):
+        '''Iteratively reflect disks in pivot, starting with index, 
+        until no more overlaps occur.'''
+        # Choose a random disk and pivot point
+        index = np.random.randint(self.N)
+        pivot = np.random.uniform(0, self.L, 2)
+        
+        movers = deque() # deque is an efficient way to manage the list of disks to move
+        movers.appendleft(index)
+        self.clear_disk(index)
+        while movers:
+            mover = movers.pop()
+            self.configuration[mover] = (2*pivot - self.configuration[mover]) % self.L # reflect disk
+            overlap = self.get_overlap(self.configuration[mover])
+            for i in overlap:
+                movers.appendleft(i)
+                self.clear_disk(i)
+            self.add_disk(mover)
+
     def evolve(self, algorithm='metropolis_hastings', steps=100):
         '''
         Perform the Metropolis-Hastings algorithm for a number of steps
@@ -95,9 +151,12 @@ class DiskModel:
         if algorithm == 'metropolis_hastings':
             for _ in range(steps):
                 self.metropolis_hastings()
+        elif algorithm == 'cluster':
+            for _ in range(steps):
+                self.disk_cluster_move()
 
 
-instance = DiskModel(50, 100)
+instance = DiskModel(500, 100)
 plot_disk_configuration(instance.configuration, instance.L)
-instance.evolve(algorithm='metropolis_hastings', steps=1000)
+instance.evolve(algorithm='cluster', steps=1000)
 plot_disk_configuration(instance.configuration, instance.L)
--- Disk Model/test_disk.py
+++ Disk Model/test_disk.py
@@ -1,8 +1,5 @@
 import numpy as np
 
 
-element = np.array([1, 2])
-array = np.array([[1, 2], [3, 4], [5, 6]])
-print(array)
-array = np.append(array, [element], axis=0)
+array = np.array([[1,2],[3,4],[5,6]])
 print(array)
